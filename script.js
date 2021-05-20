68890e06df7b1da884667b89b2ae27fc7393bb9e
Sergey Makarenko <sergey.makarenko@startinns.com>
2021-05-20 18:40:09 +0300

    Added classes

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-    { title: 'Dress', price: 200 },
-    { title: 'Blazer ', price: 180 },
-    { title: 'Vest ', price: 75 },
-    { title: 'Sweater ', price: 250 },
-  ];
-  
-const renderGoodsItem = (title = 'Товар еще не добавлен', 
-    price = 0) => `<div class="goods-item"><h3>${title}</h3><p>Price: ${price}$</p></div>`;
-  
-const renderGoodsList = (arr = 'Корзина временно недоступна') => {
-    if (Array.isArray(arr)) {
-        arr.forEach(elem => {
-            document.querySelector('.goods-list').innerHTML += renderGoodsItem(elem.title, elem.price);
-        });
-    } else {
-        document.querySelector('.goods-list').innerHTML = arr;
+class GoodsItem {
+    constructor(title = 'Товар еще не добавлен', price = 0) {
+        this.title = title;
+        this.price = price;
     }
-};
+
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>Price: ${this.price}$</p></div>`;
+    }
+}
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+            { title: 'Dress', price: 200 },
+            { title: 'Blazer ', price: 180 },
+            { title: 'Vest ', price: 75 },
+            { title: 'Sweater ', price: 250 },
+        ];
+    }
+
+    render() {
+        let htmlResult = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            htmlResult += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = htmlResult;
+    }
+
+    totalСost() {
+        let sum = 0;
+        for (let good of this.goods) {
+            sum += good.price;
+        }
+        console.log(sum);
+        return sum;
+    }
+}
+
+class Basket extends GoodsList {
+    constructor(...args) {
+        super(...args);
+    }
   
-renderGoodsList(goods);
+    addProduct() {}
+
+    deleteProduct() {}
+
+    filteringByPrice() {}
+}
+  
+class BasketItem extends GoodsItem {
+    constructor(...args) {
+        super(...args);
+        this.totalQantity = 0;
+    }
+}
+
+const productsList = new GoodsList();
+productsList.fetchGoods();
+productsList.render();
+productsList.totalСost();
